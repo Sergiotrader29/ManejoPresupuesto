d02cc5360ccd3ffaed96a81981bfeb94cb8b449c
Sergio vasquez <103544569+Sergiotrader29@users.noreply.github.com>
2023-02-14 12:37:32 -0500

    Formulario

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,25 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController: Controller // Ctrl . para importar AspNetcore
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuentas tipoCuentas)
+        {
+            if (!ModelState.IsValid) // si el modelo es ! no valido 
+            {
+                return View(tipoCuentas); //Que retorne de nuevo, y que no borre los datos
+            }
+
+
+            return View();
+        }
+    }
+}
---
+++ ManejoPresupuesto/Models/TipoCuentas.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuentas
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo nombre es requerido")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int OrdenId { get; set; }
+
+    }
+
+}
